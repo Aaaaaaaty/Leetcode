ca899f93d0211690c2ba96ef8f74e346df819249
An <2103887953@qq.com>
2017-08-07 20:45:58 +0800

    #53

--- Array/MaximumSubarray.js
+++ Array/MaximumSubarray.js
@@ -1,3 +1,9 @@
+/*
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+*/
 var maxSubArray = function(nums) {
     var max=nums[0], mid=nums[0];
     for (var i=1;i<nums.length;++i){
